260e29d48681d3c038bd0b227015bff57d979cdd
Alan Verbner <alverbner@gmail.com>
2025-09-04 19:37:51 -0300

    Enhance auth module with password validation and logout

--- auth.py
+++ auth.py
@@ -3,8 +3,13 @@ class AuthManager:
         self.users = {}
     
     def register(self, username, password):
+        if len(password) < 8:
+            raise ValueError("Password too short")
         self.users[username] = password
         return True
     
     def login(self, username, password):
         return self.users.get(username) == password
+    
+    def logout(self, username):
+        return True
